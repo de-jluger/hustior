39cf9d84f03aebf9aec39627ed392db757c0823e
Jörg Luger <joerg.luger@gmail.com>
2017-12-31 19:13:55 +0100

    Init.

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# Table of Contents
+1. [How to build](#how-to-build)
+2. [What does it do](#What-does-it-do)
+3. [When to use](#third-example)
+4. [When not to use](#when-not-to-use)
+5. [Limits](#limits)
+
+
+## How to build
+go build hustior.go
+
+
+## What does it do?
+hustior tries to hide all of your home directory except the folders you specify as the program arguments and it will hide all other processes.  
+If you call hustior with no arguments it will start a bash and you will be in your home directory. But your home directory will be empty. Don't worry your file won't be deleted. They are just not visible from this bash (and all their child processes).  
+When you call hustior with the argument "/home/<user>/path1/path2" it will be the same as above except that you have one directory in your home: "path2". "path2" will contain all the subdirectories and files that it also contains outside of the hustior started bash.  
+When stopping the hustior started bash all files saved in the home will disappear. Except they were stored in a mounted directory. Like e.g. "path2" from the example above.
+
+## When to use
+Use it when you want to get an extra line of defense before executing software or surfing on a site when everything should be OK. E.g. you start software like NodeJS, VisualCode, Eclipse, ... All this software is created by well known companies/organisations and used by millions of people. It should be safe to use. But one malicious/infected plugin and an attacker has access to all your mails/picutures/videos/documents. See this [XKCD](#https://xkcd.com/1200/).  
+The idea of hustior is to allow programs like Eclipse only access to its binary and its workspace. There is no need to access my mails or my holiday pictures.
+
+## When not to use
+hustior is not battle hardened. Do not use it when you expect to get attacked. E.g. you execute a script/binary downloaded from a shady website.  
+In this case use a virtual machine on a spare PC that you nuke regularly.  
+Also don't call hustior as root. You should have absolutely no doubt about the software you execute as root.
+
+## Limits
+The temp filesystem used is only 200MB. Storing more in your home isn't possible. When you want to store larger files give a directory as an argument and store the files there.  
+The started programs have full network access and can even access ports opened by other processes.  
+The processes started by the hustior bash aren't isolated from attacks outside. E.g. an attacker made it to your machine and runs in your user context (not restricted by hustior) and you start a browser from the hustior bash. The attacker can see the browser.  
+Setuid won't work. As a normal user you can only map your own user id. Per default "[an unmapped user ID is converted to the overflow user ID](#http://man7.org/linux/man-pages/man7/user_namespaces.7.html)". So in the restarted hustior all files that belonged to root will belong to nobody (when nobody has the overflow user ID). I haven't found a way to fix this when starting the bash. So in the bash the setuid programs like ping or Chrome sandbox won't start as the don't belong to root.  
+Please also note that hustior is currently tested only on Ubuntu 17.10.
---
+++ hustior.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	userJson := flag.String("user","","")
+	flag.Parse()
+	if userJson==nil || *userJson=="" {
+		restartInNamespace()
+		return
+	}
+	user := user.User{}
+	err := json.Unmarshal([]byte(*userJson),&user)
+	if err!=nil {
+		log.Println("Wrong user provided. Please don't use the argument manually")
+		return
+	}
+	rootBase := setUpNewRootFS()
+	setUpHomeDirectory(user,rootBase)
+	startCommand(rootBase,user)
+}
+
+//Restarts this application in a new user, mount and pid namespace while the user id
+//of the caller is mapped to 0.
+func restartInNamespace() {
+	user,err := user.Current()
+	onErrorLogAndExit(err)
+	if user.HomeDir == "/root" {
+		log.Println("Don't call as root")
+		return
+	}
+	userData,err := json.Marshal(user)
+	onErrorLogAndExit(err)
+	cmd := exec.Command(os.Args[0], append([]string{"-user",string(userData)},flag.Args()...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID: os.Getuid(),
+				Size: 1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap {
+			{
+				ContainerID: 0,
+				HostID: os.Getgid(),
+				Size: 1,
+			},
+		},
+	}
+	onErrorLogAndExit(cmd.Run())
+}
+
+//Sets up a new root filesystem that the sandbox should use.
+//The return value is the location of the new root.
+func setUpNewRootFS() (rootBase string) {
+	err := syscall.Mount("none","/","",syscall.MS_REC|syscall.MS_PRIVATE,"")
+	onErrorLogAndExit(err)
+	err = syscall.Mount("none","/root","tmpfs",0,"size=200M")
+	onErrorLogAndExit(err)
+	rootBase = "/root/base"
+	devDir := rootBase+"/dev"
+	procDir := rootBase+"/proc"
+	createDirs := []string{rootBase,devDir,rootBase+"/run",procDir,rootBase+"/tmp"}
+	for _,dir := range createDirs {
+		onErrorLangAndExitWithDesc(syscall.Mkdir(dir,0755),dir)
+	}
+	bindDirs := []string{"/bin","/etc","/lib","/lib64","/opt","/sbin","/usr","/var","/dev/shm","/run/resolvconf","/run/user"}
+	for _,dir := range bindDirs {
+		path := rootBase+dir
+		onErrorLangAndExitWithDesc(syscall.Mkdir(path,0755),path)
+		onErrorLangAndExitWithDesc(syscall.Mount(dir, path, "", syscall.MS_REC|syscall.MS_BIND, ""),path)
+	}
+	devFiles := []string{"random","urandom","null","zero"}
+	for _,devFile := range devFiles {
+		onErrorLangAndExitWithDesc(mountBindDevDir(devDir,devFile),"/dev/"+devFile)
+	}
+	//The mount of /proc is currently necessary or I will get a "fork/exec /bin/bash: permission denied"
+	err = syscall.Mount("proc","/proc","proc",syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV,"")
+	onErrorLogAndExit(err)
+	err = syscall.Mount("proc",procDir,"proc",syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV,"")
+	onErrorLogAndExit(err)
+	return
+}
+
+//Takes the unnamed arguments as directories that are bound under <rootBase>/home/<user>/
+func setUpHomeDirectory(user user.User,rootBase string) {
+	newHomeDir := rootBase+user.HomeDir
+	onErrorLogAndExit(os.MkdirAll(newHomeDir,0700))
+	for _,p := range flag.Args() {
+		dirName := path.Base(p)
+		absDirName := newHomeDir+"/"+dirName
+		onErrorLogAndExit(syscall.Mkdir(absDirName,0700))
+		onErrorLogAndExit(syscall.Mount(p, absDirName, "", syscall.MS_BIND|syscall.MS_REC, ""))
+	}
+	return
+}
+
+//Starts a bash. The bash runs in a new user namespace where the root id is mapped back to the original user id and
+//the bash is chrooted in the given rootBase.
+func startCommand(rootBase string, user user.User) {
+	uid, err := strconv.Atoi(user.Uid)
+	onErrorLogAndExit(err)
+	gid, err := strconv.Atoi(user.Gid)
+	onErrorLogAndExit(err)
+	cmd := exec.Command("/bin/bash")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = user.HomeDir
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Chroot: rootBase,
+		Cloneflags: syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: uid,
+				HostID: os.Getuid(),
+				Size: 1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap {
+			{
+				ContainerID: gid,
+				HostID: os.Getgid(),
+				Size: 1,
+			},
+		},
+	}
+	onErrorLogAndExit(cmd.Run())
+}
+
+//Creates the given device file in the given dev dir and then bind the device file from /dev to it.
+func mountBindDevDir(devDir,deviceName string) (error) {
+	devicePath := devDir+"/"+deviceName
+	deviceFile,err := os.OpenFile(devicePath, os.O_RDONLY|os.O_CREATE, 0666)
+	if err!=nil { return err }
+	deviceFile.Close()
+	err = syscall.Mount("/dev/"+deviceName, devicePath, "", syscall.MS_BIND, "")
+	return err
+}
+
+// When the given error is not nil then print it and exit the appication.
+func onErrorLogAndExit(e error) {
+	if e!=nil {
+		log.Fatal(e)
+	}
+}
+
+// When the given error is not nil then print it together with the description and exit the appication.
+func onErrorLangAndExitWithDesc(e error, description string) {
+	if e!=nil {
+		log.Fatal(description, e)
+	}
+}
+
